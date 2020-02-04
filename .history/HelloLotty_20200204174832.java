ef9e6b6c7d942c5cbf5637f4ebc74fc7ce9661d0
lottenoorlander <lottenoorlander@gmail.com>
2020-02-04 18:24:57 +0100

    fixed commit

--- .history/HelloLotty_20200204174832.java
+++
@@ -1,9 +0,0 @@
-public class HelloLotty {
-    public static void main(String[] arg){
-        System.out.println("Hello Lotty!");
-        String name = "Lotty";
-        int age = 31;
-        System.out.println(name);
-        System.out.println(age);
-    }
-}
