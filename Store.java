7bcd519dbee6158a54e8ef3cc731966b90ab0ecb
lottenoorlander <lottenoorlander@gmail.com>
2020-02-06 13:05:36 +0100

    learned about classes

---
+++ Store.java
@@ -0,0 +1,17 @@
+public class Store {
+
+    String productType;
+    int inventoryCount;
+    double inventoryPrice;
+
+    public Store(String product, int count, double price) {
+        productType = product;
+        inventoryCount = count;
+        inventoryPrice = price;
+    }
+
+    public static void main(String[] args) {
+        Store cookieShop = new Store("cookies", 12, 3.75);
+        System.out.println(cookieShop.productType);
+    }
+}
