70fc134ff56ed76f4ca4a7878fe3f09b2f775856
lottenoorlander <lottenoorlander@gmail.com>
2020-02-08 11:58:06 +0100

    learned about if then else statements in java

---
+++ ControlFlow.java
@@ -0,0 +1,11 @@
+public class ControlFlow {
+    public static void main(String[] args) {
+        boolean isFilled = false;
+
+        if (isFilled) {
+            System.out.println("Shipping");
+        } else {
+            System.out.println("Order not ready");
+        }
+    }
+}
